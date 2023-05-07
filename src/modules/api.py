3b4131753d2a98db4b2654a498047e4491b04900
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2023-05-08 00:19:15 +0300

    Newspaper API

--- src/modules/api.py
+++ src/modules/api.py
@@ -25,14 +25,79 @@ This module is not complete.
 
 
 # ------- Libraries and utils -------
-from flask import Blueprint
+import bleach
+from flask import Blueprint, abort, render_template, request, url_for
+from init import db
+from modules.database import Newspaper
 
 
 # ------- Blueprint init -------
 api = Blueprint("api", __name__, template_folder="../templates", static_folder="../static")
 
 
+# ------- API models -------
+
+# ---- Newspaper model ----
+class NewspaperApi():
+    file_datetime: str 
+    filename: str
+    pdf_view_url: str
+    pdf_download_url: str
+    publication_num: int
+    date: str 
+    credits: str
+
+    def __init__(self, file_datetime: str, filename: str, pdf_view_url: str, pdf_download_url: str, publication_num: int, date: str, credits: str):
+        self.file_datetime = file_datetime
+        self.fliename = filename
+        self.pdf_view_url = pdf_view_url
+        self.pdf_download_url = pdf_download_url
+        self.publication_num = publication_num
+        self.date = date
+        self.credits = credits
+
+
 # ------- Page routes -------
 @api.route("/")
 def index():
-    return {"status": 503, "data": "API not ready."}, 503
+    return render_template("api_index.html")
+
+
+@api.route("/documentation/v1")
+def v1_documentation():
+    return render_template("api/documentation.html")
+              
+              
+@api.route("/v1/get/newspaper/<filter>")
+def v1_get_newspaper(filter):
+    query = request.args.get("query")
+    
+    if filter == "archive":
+        query = db.session.query(Newspaper).all()
+        ret = []
+        query.reverse()
+        
+        for query in query:
+            ret.append(NewspaperApi(query.file_datetime, f"pub_{query.file_datetime}.pdf", url_for("newspaper_pages.view_pub", date_time=query.file_datetime), url_for("newspaper_pages.download_pub", date_time=query.file_datetime), query.id, query.date, query.credits).__dict__)
+        
+        return ret
+    
+    elif filter == "date" and query:
+        query = db.session.query(Newspaper).filter_by(date=bleach.clean(query.replace("-", "/"))).first()
+        
+        if query:
+            return NewspaperApi(query.file_datetime, f"pub_{query.file_datetime}.pdf", url_for("newspaper_pages.view_pub", date_time=query.file_datetime), url_for("newspaper_pages.download_pub", date_time=query.file_datetime), query.id, query.date, query.credits).__dict__
+        
+        else:
+            return {}
+    
+    elif filter == "pub_num" and query:
+        query = db.session.query(Newspaper).filter_by(id=bleach.clean(query)).first()
+        
+        if query:
+            return NewspaperApi(query.file_datetime, f"pub_{query.file_datetime}.pdf", url_for("newspaper_pages.view_pub", date_time=query.file_datetime), url_for("newspaper_pages.download_pub", date_time=query.file_datetime), query.id, query.date, query.credits).__dict__
+        
+        else:
+            return {}
+    
+    abort(404)
